a6b3dd1fe982fe098e31b010d08e11463fc84ecc
SainasFede <sainasfede@gmail.com>
2022-10-13 16:24:52 +0200

    add struttura

---
+++ script/script.js
@@ -0,0 +1,42 @@
+const container = document.querySelector('.container');
+const limitBox = 100
+const fizz = 'Fizz'
+const buzz = 'Buzz'
+const fizzBuzz = 'FizzBuzz'
+
+
+for(let i = 1; i <= limitBox; i++){
+  const box = document.createElement('div');
+  box.className = 'box';
+
+  if(!(i % 3)){
+    box.classList.add('yellow')
+    box.innerHTML = fizz
+    
+
+  }if(!(i % 5)){
+    box.classList.add('blue')
+    box.innerHTML = buzz
+
+  }if(!(i % 15)){
+    box.classList.add('green')
+    box.innerHTML = fizzBuzz
+  }
+  
+
+
+  box.innerHtml = i;
+  container.append(box);
+}
+
+
+
+
+
+
+
+
+
+
+
+
