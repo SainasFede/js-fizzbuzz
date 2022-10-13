4681e710a7de5918720307d52e763b348b26dfa3
SainasFede <sainasfede@gmail.com>
2022-10-13 16:39:55 +0200

    add esercizio

--- script/script.js
+++ script/script.js
@@ -8,25 +8,19 @@ const fizzBuzz = 'FizzBuzz'
 for(let i = 1; i <= limitBox; i++){
   const box = document.createElement('div');
   box.className = 'box';
+  box.innerHTML = i;
+  container.append(box);
 
   if(!(i % 3)){
     box.classList.add('yellow')
     box.innerHTML = fizz
-    
-
   }if(!(i % 5)){
     box.classList.add('blue')
     box.innerHTML = buzz
-
   }if(!(i % 15)){
     box.classList.add('green')
     box.innerHTML = fizzBuzz
   }
-  
-
-
-  box.innerHtml = i;
-  container.append(box);
 }
 
 
